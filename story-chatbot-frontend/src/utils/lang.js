f407391fcdc01801190125b31dc353ef3fac1509
Valentin Rudloff <valentin.rudloff.perso@gmail.com>
2024-11-02 14:39:17 +0100

    add more lang

--- story-chatbot-frontend/src/utils/lang.js
+++ story-chatbot-frontend/src/utils/lang.js
@@ -3,14 +3,26 @@ const resources = {
     translation: {
       "stage_start_title": "Dive into a world of mysteries and adventures with our narrative exploration game, where you are the hero! By making choices at each stage, you’ll guide the main character through puzzles, trials, and captivating quests in mystical locations.",
       "stage_start_subtitle": "Good luck!",
-      "stage_start_button": "Start"
+      "stage_start_button": "Start",
+      "feedback_q1": "How was the story?",
+      "feedback_q2": "How was the character?",
+      "feedback_q3": "Any other feedbacks?",
+      "feedback_q3_placeholder": "Please share your thoughts and suggestions here...",
+      "feedback_email": "Your email:",
+      "feedback_submit": "Play Again"
     }
   },
   fr: {
     translation: {
       "stage_start_title": "Plongez dans un monde de mystères et d’aventures avec ce jeu d’exploration narratif, où vous êtes le héros ! En faisant vos choix à chaque étape, vous guiderez le personnage principal à travers des énigmes, des épreuves et des quêtes fascinantes dans des lieux mystiques.",
       "stage_start_subtitle": "Bonne chance !",
-      "stage_start_button": "Commencer"
+      "stage_start_button": "Commencer",
+      "feedback_q1": "Comment était la histoire?",
+      "feedback_q2": "Comment était le personnage?",
+      "feedback_q3": "D'autres commentaires?",
+      "feedback_q3_placeholder": "Veuillez partager vos pensées et suggestions ici...",
+      "feedback_email": "Votre email:",
+      "feedback_submit": "Rejouer"
     }
   }
 };
